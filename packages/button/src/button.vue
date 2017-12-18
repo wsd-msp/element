40e1286c12798d7481b949f43dab6abe8292a134
Mathias Petersen <msp@westsoftdevelopment.dk>
2017-12-18 10:58:27 +0100

    Prevent click in code when disabled

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,9 +1,9 @@
 <template>
-  <button :disabled="disabled" class="el-button"
-    @click="handleClick"
-    :autofocus="autofocus"
-    :type="nativeType"
-    :class="[
+	<button :disabled="disabled" class="el-button"
+			@click="handleClick"
+			:autofocus="autofocus"
+			:type="nativeType"
+			:class="[
       type ? 'el-button--' + type : '',
       size ? 'el-button--' + size : '',
       {
@@ -12,11 +12,11 @@
         'is-plain': plain
       }
     ]"
-  >
-    <svg-icon class="el-icon-loading" icon="loading" v-if="loading" @click="handleInnerClick"></svg-icon>
-    <svg-icon :class="'el-icon-'" :icon="icon" v-if="icon && !loading" @click="handleInnerClick"></svg-icon>
-    <span v-if="$slots.default" @click="handleInnerClick"><slot></slot></span>
-  </button>
+	>
+		<svg-icon class="el-icon-loading" icon="loading" v-if="loading" @click="handleInnerClick"></svg-icon>
+		<svg-icon :class="'el-icon-'" :icon="icon" v-if="icon && !loading" @click="handleInnerClick"></svg-icon>
+		<span v-if="$slots.default" @click="handleInnerClick"><slot></slot></span>
+	</button>
 </template>
 <script>
   export default {
@@ -44,7 +44,9 @@
 
     methods: {
       handleClick(evt) {
-        this.$emit('click', evt);
+        if (!this.disabled) {
+          this.$emit('click', evt);
+        }
       },
       handleInnerClick(evt) {
         if (this.disabled) {
